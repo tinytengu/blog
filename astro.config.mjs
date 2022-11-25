c93e842da4ccbc87ca02b91c79692ed228d9b37c
tinytengu <tinytengu@yandex.ru>
2022-11-26 00:11:12 +0300

    Changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,8 +13,8 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), astroI18next()],
-  site: "https://tinytengu.github.io",
-  base: "/blog",
+  site: "https://tinyblog-tinytengu.vercel.app",
+  base: "/",
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });

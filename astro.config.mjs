076c9ed8725761ef3f39e9d9923f94b0d3db1c53
tinytengu <tinytengu@yandex.ru>
2022-11-26 00:04:45 +0300

    An attempt to implement Vercel SSR integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
-
 import astroI18next from "astro-i18next";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), astroI18next()],
   site: "https://tinytengu.github.io",
   base: "/blog",
+  output: "server",
+  adapter: vercel()
 });

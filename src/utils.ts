e8140813a6d5f91f946367caa8acb2a71fc168db
tinytengu <tinytengu@yandex.ru>
2022-11-25 23:36:13 +0300

    Article languages resolution based on present files now

--- src/utils.ts
+++ src/utils.ts
@@ -27,3 +27,14 @@ export function parseArticleURL(url: string): {
     language: match[2],
   };
 }
+
+// Group objects array by property
+export function groupBy<T>(
+  array: T[],
+  predicate: (value: T, index: number, array: T[]) => string
+) {
+  return array.reduce((acc, value, index, array) => {
+    (acc[predicate(value, index, array)] ||= []).push(value);
+    return acc;
+  }, {} as { [key: string]: T[] });
+}
